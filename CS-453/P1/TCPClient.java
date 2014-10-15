70548e619a277d11ae85e537ffe86b720b080f44
Nam Phan <namp@umass.edu>
2014-10-15 12:16:58 -0400

    testing P1

--- CS-453/P1/TCPClient.java
+++ CS-453/P1/TCPClient.java
@@ -22,15 +22,20 @@ public class TCPClient{
 		response = "";
 		
 		
-		//container = new 
+		//container = new need to get size!
 	
 	}
 	
-	public int getBlockSize(){ //gets the size of the block from the reply
+	public void splitHeader(){ // split header by with delimiter: \n\n
 		
-		
+	}
+	
+	
+	public int getBlockSize(){ //gets the size of the block from the reply header
+
 		return 0;
 	}
+	
 	public void talkToLocalServer() throws UnknownHostException, IOException{
  
 
@@ -51,67 +56,75 @@ public class TCPClient{
 		clientSocket.close();
 	}
 	
-	public void talkToPlumServer() throws IOException{
-		// needs to use GET Redsox.jpg\n
-		request = "GET Redsox.jpg";
-		Image image = null;
-		byte serverR;
-		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); //creates an obj to send
+	public void talkToTestServer() throws UnknownHostException, IOException{
+		request = "IAM 25004174";
 		
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); //creates an obj to send
 		outToServer.writeBytes(request + '\n'); // sends to the server
 		
-		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); //buffer for server reply
-		
-		// reads the whole thing >.<
-		//response = inFromServer.readLine();  // reads from server buffer
-		
-		// probably need to parse this
-//		200 OK		-> status code
-//		BODY_BYTE_OFFSET_IN_FILE: 0 	-> byte off set(always 0 for CS)
-//		BODY_BYTE_LENGTH: 58241			-> byte length ()
-//		\n\n							-> two new lines to separate header from payload
-//		<bytes of body follow here>
-//		
-//		note: for client server, the entire file in the body
-
-		//use this code
-		//URL url = new URL("http://www.yahoo.com/image_to_read.jpg"); // delete
-		
-				
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		
-		byte[] buf = new byte[60000];
-		
-		int n = 0;
-		while(inFromServer.readLine() != null){
-			inFromServer.readLine();	
-			out.write(buf,0,n);
-			n++;
-		}
-		
-		// while there are still stuff in the bufer...
-//		while (-1 != (n=inFromServer.readLine()))
-//		{
-//		   out.write(buf, 0, n);
-//		}
-		
-		
-		out.close();
+		outToServer.close(); // closing server stream
 		clientSocket.close();
-		byte[] response = out.toByteArray();
-		
-		// saving the image
-		//http://stackoverflow.com/questions/5882005/how-to-download-image-from-any-web-page-in-java
-//		FileOutputStream fos = new FileOutputStream("C://borrowed_image.jpg");
-//		fos.write(response);
-//		fos.close();
-
-		clientSocket.close();
+	}
+	
+	public void talkToPlumServer() throws IOException{
+	 //... I like pears
 		
 	}
 	
 	public void talkToPearServer() throws IOException{
-		
+		// needs to use GET Redsox.jpg\n
+				request = "GET Redsox.jpg";
+				byte serverR;
+				DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); //creates an obj to send
+				
+				outToServer.writeBytes(request + '\n'); // sends to the server
+				
+				//BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); //buffer for server reply
+				
+				// reads the whole thing >.<
+				//response = inFromServer.readLine();  // reads from server buffer
+				
+				// probably need to parse this
+//				200 OK		-> status code
+//				BODY_BYTE_OFFSET_IN_FILE: 0 	-> byte off set(always 0 for CS)
+//				BODY_BYTE_LENGTH: 58241			-> byte length ()
+//				\n\n							-> two new lines to separate header from payload
+//				<bytes of body follow here>
+//				
+//				note: for client server, the entire file in the body
+
+				//use this code
+				//URL url = new URL("http://www.yahoo.com/image_to_read.jpg"); // delete
+				
+						
+//				ByteArrayOutputStream out = new ByteArrayOutputStream();
+//				
+//				byte[] buf = new byte[60000];
+//				
+//				int n = 0;
+//				while(inFromServer.readLine() != null){
+//					inFromServer.readLine();	
+//					out.write(buf,0,n);
+//					n++;
+//				}
+				
+				// while there are still stuff in the bufer...
+//				while (-1 != (n=inFromServer.readLine()))
+//				{
+//				   out.write(buf, 0, n);
+//				}
+				
+				
+				//out.close();
+				clientSocket.close();
+				//byte[] response = out.toByteArray();
+				
+				// saving the image
+				//http://stackoverflow.com/questions/5882005/how-to-download-image-from-any-web-page-in-java
+//				FileOutputStream fos = new FileOutputStream("C://borrowed_image.jpg");
+//				fos.write(response);
+//				fos.close();
+
 	}
 
 	public static void main(String args[]) throws Exception {
@@ -123,10 +136,12 @@ public class TCPClient{
 //		
 //		if(mode.toUpperCase().equals("CS")){ // client-server
 //			if((	ip.toLowerCase().equals("pear.cs.umass.edu") || 
-//					ip.toLowerCase().equals("plum.cs.umass.edu") || 
-//					ip.toLowerCase().equals("date.cs.umass.edu")) && port == 18765){
+//					ip.toLowerCase().equals("plum.cs.umass.edu")) && port == 18765){
 //						
 //				client.talkToPearServer();
+//			}
+//			else if (ip.toLowerCase().equals("date.cs.umass.edu") && port == 20001){
+//				client.talkToTestServer();
 //			}
 //			else if(ip.toLowerCase().equals("localhost") && port == 6789){
 //				client.talkToLocalServer();
@@ -146,9 +161,11 @@ public class TCPClient{
 		TCPClient toLocal = new TCPClient ("localhost", 6789);
 		TCPClient toPear = new TCPClient("pear.cs.umass.edu", 18765);
 		TCPClient toPlum = new TCPClient("plum.cs.umass.edu", 18765);
-		TCPClient toTest = new TCPClient("date.cs.umass.edu", 19876); // used to test the server
+		TCPClient toTest = new TCPClient("date.cs.umass.edu", 20001); //19876 = UDP port // used to test the server
 		
+		toPear.talkToPearServer();
 		
+		//toTest.talkToTestServer();
 		//toLocal.talkToLocalServer();
 		
 
