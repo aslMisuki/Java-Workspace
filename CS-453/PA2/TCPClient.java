c393d7aeefa5ca1d5224be0a5e4fc84c2fd5af2a
Nam Phan <namp@umass.edu>
2014-11-04 07:26:15 -0500

    updating

--- CS-453/PA2/TCPClient.java
+++ CS-453/PA2/TCPClient.java
@@ -1,12 +1,13 @@
 package PA2;
 
 import javax.imageio.ImageIO;
+
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.*; 
 import java.net.*;
 
-public class TCPClient{
+public class TorrentClient{
 
 	//header data
 	int bodyByteOffset,bodyByteLength;
@@ -18,14 +19,14 @@ public class TCPClient{
 	Socket clientSocket;
 	Socket peerSocket;
 
-	public TCPClient(String hostname, int port) throws UnknownHostException, IOException{
+	public TorrentClient(String hostname, int port) throws UnknownHostException, IOException{
 		clientSocket = new Socket(hostname, port); //creates a socket to use
 		this.hostname = hostname;
 		this.port = port;
 
 	}
-	
-	public TCPClient(){} // empty client overload
+
+	public TorrentClient(){} // empty client overload
 
 	// parses the header and returns false if not end of header, and true otherwise
 	private boolean parseHeaderLine(String line){
@@ -88,63 +89,119 @@ public class TCPClient{
 		inFromServer.close();
 		clientSocket.close();
 	}
+	public void talkToPA1() throws IOException{
+		request = "Redsox.jpg";
+		System.out.println("Sending Get Request");
+
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		//send request to server
+		System.out.println("Writing request out to server: " + "GET: " +request+ '\n' );
+		outToServer.writeBytes("GET "+request+ '\n');
+		outToServer.flush();
+
+		System.out.println("Getting InputStream from Server");
+
+		int bSize = 0;
+		boolean eoh = false; // boolean for end of file
+		String[] lengthArray;
+
+		DataInputStream inFromServer = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+
+		System.out.println(inFromServer.readLine()); // status
+
+		while(!eoh){
+			eoh = parseHeaderLine(inFromServer.readLine());
+		}
+
+		Socket peerSocket = new Socket("date,cs.umass.edu",19876);
+
+
+		byte[] buffer = new byte[1024*5];
+
+
+		//writing to file
+		System.out.println("Writing to .jpg File now");
+		FileOutputStream fileOut = new FileOutputStream("Redsox.jpg");
+
+		BufferedOutputStream fileBuffer = new BufferedOutputStream(fileOut);
+		System.out.println("Making .jpg File");
+
+		int count;
+
+		while ((count = inFromServer.read(buffer)) != -1) { // while not end of datastream
+			fileBuffer.write(buffer, 0, count); // remember that count can be less than buffer.length
+			System.out.println("Data Received : " + count);
+			fileBuffer.flush();
+		}
+
+		fileBuffer.close();
+		fileOut.close();
+		// just closing the socket
+		System.out.println("Closing outToServer");
+		outToServer.close();
+		System.out.println("Closing inFromServer");
+		inFromServer.close();
+		System.out.println("Closing clientSocket");
+		clientSocket.close();
+	}
 
 	public void talkToPearServer() throws IOException{
 
 		System.out.println("Sending IAM Request");
-		
+
 		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 		DataInputStream inFromServer = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
-		
+
 		//contact grading server
 		System.out.println("contacting grading server: " + hostname +":"+ port);
 		request = "IAM 25004174";
-		
+
 		outToServer.writeBytes(request + '\n');
 		outToServer.flush();
-		
+
 		System.out.println("IAM response: \n" + inFromServer.readLine()); 
-		
+
 		//requestig metadata
 		request = "Redsox.jpg";
 		System.out.println("Opening Socket for MetaData: " + hostname + ":" + 19876);
-		
-		Socket peerSocket = new Socket(hostname, 19876); // connection refused
-		
+
+		Socket peerSocket = new Socket("date.cs.umass.edu",19876); // connection refused
+		System.out.println("Socket Opened");
+
 		System.out.println("Writing request for MetaData: " + "GET: " +request+ ".torrent" + '\n');
 		System.out.println("Writing request to " + hostname + ":" + 19876);
-		
-		
+
+
 		DataOutputStream outToPeer = new DataOutputStream(peerSocket.getOutputStream());
 		DataInputStream inFromPeer = new DataInputStream(new BufferedInputStream(peerSocket.getInputStream()));
-		
+
 		System.out.println("GET "+request+".torrent\n");
 		outToPeer.writeBytes("GET "+request+".torrent\n");
 		outToPeer.flush();
 
 		System.out.println("Getting InputStream(metadata) from Server");
-		System.out.println(inFromPeer.readLine());
-		
+		System.out.println(inFromServer.readLine());
+
 		outToPeer.close();
 		inFromPeer.close();
 		peerSocket.close();
 
-//		int bSize = 0;
-//		boolean eoh = false; // boolean for end of file
-//		boolean eOMD = false;
-//		MetaData meta = new MetaData(); // calling this multiple times will yield different peers lets start with calling this 3 times
-//		String[] lengthArray;    
-		
-//		while(!eOMD){
-//			System.out.println("trying to parse metadata");
-//			eOMD = meta.parseMetaData(inFromServer.readLine());
-//		}
-//		
-//		System.out.println(meta.toString());
-		
+		//		int bSize = 0;
+		//		boolean eoh = false; // boolean for end of file
+		//		boolean eOMD = false;
+		//		MetaData meta = new MetaData(); // calling this multiple times will yield different peers lets start with calling this 3 times
+		//		String[] lengthArray;    
+
+		//		while(!eOMD){
+		//			System.out.println("trying to parse metadata");
+		//			eOMD = meta.parseMetaData(inFromServer.readLine());
+		//		}
+		//		
+		//		System.out.println(meta.toString());
+
 		//TODO: we are going to assume this works, meta data is retrieved and parsed
-		
-		
+
+
 		//now we need to store the meta data 
 		//status = inFromServer.readLine(); // should be the status
 
@@ -153,22 +210,22 @@ public class TCPClient{
 		//			eoh = parseHeaderLine(inFromServer.readLine());
 		//		}
 
-//		byte[] buffer = new byte[1024*5];
-//
-//		System.out.println("Writing to .jpg File now");
-//		FileOutputStream fileOut = new FileOutputStream("Redsox.jpg");
-//
-//		BufferedOutputStream fileBuffer = new BufferedOutputStream(fileOut);
-//		System.out.println("Making .jpg File");
-//
-//		int count;
-//
-//		while ((count = inFromServer.read(buffer)) != -1) { // while not end of file
-//
-//			fileBuffer.write(buffer, 0, count);
-//			System.out.println("Data Received : " + count);
-//			fileBuffer.flush();
-//		}
+		//		byte[] buffer = new byte[1024*5];
+		//
+		//		System.out.println("Writing to .jpg File now");
+		//		FileOutputStream fileOut = new FileOutputStream("Redsox.jpg");
+		//
+		//		BufferedOutputStream fileBuffer = new BufferedOutputStream(fileOut);
+		//		System.out.println("Making .jpg File");
+		//
+		//		int count;
+		//
+		//		while ((count = inFromServer.read(buffer)) != -1) { // while not end of file
+		//
+		//			fileBuffer.write(buffer, 0, count);
+		//			System.out.println("Data Received : " + count);
+		//			fileBuffer.flush();
+		//		}
 
 		// just closing the socket
 		System.out.println("Closing outToServer");
@@ -191,14 +248,14 @@ public class TCPClient{
 		String mode = "";
 		String ip = "";
 		int port = -1;
-		TCPClient client = new TCPClient();
+		TorrentClient client = new TorrentClient();
 
 		switch(run){
 		case "shell":
 			mode = args[0]; // will have 3 parts, Mode, IP, Port
 			ip = args[1];
 			port = Integer.parseInt(args[2]);
-			client = new TCPClient(ip, port);
+			client = new TorrentClient(ip, port);
 			break;
 		case "IDE":
 			mode = "P2P";
@@ -233,8 +290,8 @@ public class TCPClient{
 				}
 				break;
 			case "test" :
-				TCPClient toPear = new TCPClient("pear.cs.umass.edu", 18765);
-				toPear.talkToPearServer();
+				TorrentClient toPear = new TorrentClient("pear.cs.umass.edu", 18765);
+				toPear.talkToPA1();
 				break;
 			default:
 				System.out.println("Wrong IP Address and/or port");
@@ -251,11 +308,11 @@ public class TCPClient{
 				}
 				break;
 			case "test":
-				TCPClient toPear = new TCPClient("pear.cs.umass.edu", 19876);
+				TorrentClient toPear = new TorrentClient("pear.cs.umass.edu", 19876);
 				toPear.talkToPearServer();
 				break;
 			case "grade" :
-				TCPClient toGrade = new TCPClient("date.cs.umass.edu", 20001);
+				TorrentClient toGrade = new TorrentClient("date.cs.umass.edu", 20001);
 				toGrade.talkToPearServer();
 				break;
 			default:
